a19e999e282a8071502cc7ad291738b63fac2a76
LuciaCarolinaRamosGa <LuciaCarolinaRamosGa@LAPTOP-422VIUHA.mshome.net>
2019-02-15 11:11:19 -0300

    demo full page

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors);
 app.use(bodyParser.json());
-//app.use(express.static(__dirname + "/public")); // load UI from public folder
+app.use(express.static(__dirname + "/public")); // load UI from public folder
 
 app.use('/api', routes);
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var server = require('./app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3014;
 
 server.listen(port, function() {
     // eslint-disable-next-line
